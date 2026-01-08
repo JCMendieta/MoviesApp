934fb57160da5e97d7e888c2a0ec0811b2a88344
camilo <juank.yoh.117@gmail.com>
2026-01-08 12:58:27 -0500

    Initial commit

---
+++ MoviesApp/MoviesApp/MoviesAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoviesAppApp.swift
+//  MoviesApp
+//
+//  Created by Juan Camilo Mendieta Hernández on 8/01/26.
+//
+
+import SwiftUI
+
+@main
+struct MoviesAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
